31bb0d7b676efe2c5022c9f443faeff50e0a8826
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2024-05-28 13:55:36 +0100

    PrimitiveTypes and referenceTypes

---
+++ PrimitiveTypes.java
@@ -0,0 +1,13 @@
+/*
+Primitive Type Variables:
+
+These are basic data types provided by Java.
+There are 8 primitive data types: byte, short, int, long, float, double, char, and boolean.
+They store the actual values.
+Examples:
+*/
+
+int age = 25l
+double price = 19.99
+
+
---
+++ ReferenceTypes.java
@@ -0,0 +1,10 @@
+/*
+Reference Type Variables
+
+These refer to objects (instances of classes).
+They store the address (reference) of the object in memory, not the actual data.
+Examples:
+*/
+
+String name = "John";
+List<Integer> numbers = new ArrayList<>();
