a800f689f127cde935b554e15753ce2ac75c7da3
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2024-05-28 14:00:21 +0100

    ActualVsFormalParameters

---
+++ ActualFormalParameter.java
@@ -0,0 +1,23 @@
+/*
+Actual vs Formal Parameter
+
+Formal Parameters:
+
+Parameters defined in the method signature.
+They act as placeholders for the values that will be passed when the method is called.
+Example:
+*/
+
+void greet(String name) {
+    System.out.println("Hello, " + name);
+}
+
+/*
+Actual Parameters (Arguments):
+
+The real values passed to the method when it is called.
+Example:
+*/
+
+greet("Alice"); // "Alice" is the actual parameter
+
---
+++ ImmutableVsMutable.java
@@ -0,0 +1,9 @@
+/*
+Actual vs Formal Parameter
+
+Formal Parameters:
+
+Parameters defined in the method signature.
+They act as placeholders for the values that will be passed when the method is called.
+Example:
+*/
