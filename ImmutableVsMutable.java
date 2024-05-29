3fe9f32d857c5f6ce3dff50ef57e8269c92961d3
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2024-05-29 19:12:55 +0100

    mutable vs immutable

--- ImmutableVsMutable.java
+++ ImmutableVsMutable.java
@@ -1,9 +1,20 @@
 /*
-Actual vs Formal Parameter
+Immutable vs Mutable Objects
 
-Formal Parameters:
-
-Parameters defined in the method signature.
-They act as placeholders for the values that will be passed when the method is called.
-Example:
+Mutable Objects
+Mutable objects are those whose state (i.e., the value of fields) can be changed after they are created. 
+Examples of mutable objects in Java include instances of classes like StringBuilder, ArrayList, and HashMap.
 */
+
+ArrayList<String> list = new ArrayList<>();
+list.add("A");
+list.add("B");
+list.set(1, "C"); // Modifies the list
+
+/*
+Immutable objects, on the other hand, cannot have their state changed after they are created. Once an immutable object is instantiated, it cannot be modified. 
+The most common example of an immutable object in Java is the String class.
+*/
+
+String str = "Hello";
+String newStr = str.concat(" World"); // Does not modify `str`, creates a new String `newStr`
